02636db41e54f48dc76b061d9001a931d4d48daf
Nnisik <nisik0120@gmail.com>
2024-05-13 20:49:09 +0300

    finished hide function

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+const animateStateCheck = document.getElementById("animation-switch"),
+    hideStateCheck = document.getElementById("hide-switch");
+
+// FIXME: creates infinate amout of error instead of rotating circle bar
+function animateCircleBar() {
+    let rotateValue = 0;
+
+    setInterval(()=>{
+        rotateValue++;
+        circularProgress.style.transform = `rotate(${rotateValue}deg)`;
+        console.log(rotateValue);
+
+        if(!animateStateCheck.checked) {
+            clearInterval(rotating);
+        }
+    }, 10);
+}
+
+function hideCircleBar() {
+    if (hideStateCheck.checked) {
+        circularProgress.style.display = "none";
+    }
+    else {
+        circularProgress.style.display = "block";
+    }
+}
+
 let circularProgress = document.getElementById("progress-circle"),
     progressValue = document.getElementById("value");
 
 let progressStartValue = 0,
-    progressEndValue = 50,
+    progressEndValue = document.getElementById("value").value,
     speed = 75;
 
-progressValue.textContent = progressEndValue;
+// progressValue.textContent = progressEndValue;
 
 let progress = setInterval(() => {
     progressStartValue++;
